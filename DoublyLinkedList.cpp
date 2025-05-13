f24ad91a1b4b44dd00d7c0c727b18c193ed876df
ranggarap <fadhilahrangga26@gmail.com>
2025-05-13 11:07:54 +0700

    locate the node to be delete

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -116,4 +116,15 @@ void deleteNode()
 
     current = START; // step 1 : start from the frist node
     previous = NULL;
+//locate the node to be delete 
+while (current !=NULL && current -> noMhs !=  rollNo)
+{
+    previous =current ;
+    current = current ->next;
+}
+if (current == NULL)
+{
+    cout << "\033 [31mthe record with roll number " << rollNo << " not found\033[0m"<< endl;
+    return;
+}
 }
