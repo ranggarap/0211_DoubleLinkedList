20c9b08708a16ee97db1c4d5342a5d686bb9e19c
ranggarap <fadhilahrangga26@gmail.com>
2025-05-13 10:54:31 +0700

    pembuatan function search untuk mencari data

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -85,7 +85,17 @@ else
         }
     }
 }
-        
-
+}
+// pembuatan function search untuk mencari data
+bool search (int rollNo, Node **previous ,Node ** current)
+{
+    *previous =NULL;
+    *current  = START;
+    while  (*current !=NULL && (*current) -> noMhs !=rollNo)
+    {
+        *previous = *current ;
+        *current = (*current)-> next;
+    }
+    return (*current !=NULL);
 
 } 
