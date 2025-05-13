53eb823178eab31dd2ca2382c6f61ee92bec475d
ranggarap <fadhilahrangga26@gmail.com>
2025-05-13 10:20:58 +0700

    insert the new node in the list

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -24,4 +24,10 @@ void addnote()
     cin >> newNode ->noMhs ;// assign value to the data field of the new node
     cout << "\nEnter the name of the student:";
     cin >> newNode -> name;//assign value to the data field of th
+    
+    //insert the new node in the list
+    // kondisi jika star = null atau noMhs node baru <= noMhs start
+    if (START == NULL || newNode -> noMhs <= START -> noMhs )
+    
+
 } 
